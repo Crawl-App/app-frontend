d3803b8862b5d0dbb68a3cba617b4385f3c4c132
Udit <udit.samant@gmail.com>
2024-08-24 16:26:38 +1000

    Create tabs

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import { Tabs } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
@@ -7,11 +6,6 @@ export default function App() {
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app! v2</Text>
             <StatusBar style="auto" />
-            <Tabs>
-                <Tabs.Screen name="index" options={{ title: "Home" }} />
-                <Tabs.Screen name="settings" options={{ title: "Settings" }} />
-                <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-            </Tabs>
         </View>
     );
 }
