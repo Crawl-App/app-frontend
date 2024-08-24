e188f5609bfe7da5467de922518298dab5a14e3b
Lancelot <lancelot.charvillat@epfl.ch>
2024-08-24 16:20:38 +1000

    map working

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Stack } from 'expo-router';
+
+const Layout = () => (
+	<Stack>
+		<Stack.Screen name="index" options={{ title: 'App' }} />
+	</Stack>
+);
+
+export default Layout;
+
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! v2</Text>
+      <Text>Open up App.js to start working on your app! v2</Text>s
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +29,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
