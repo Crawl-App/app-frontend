a965bfc2487c3995afe28fadcae783df3f68b606
Udit <udit.samant@gmail.com>
2024-08-25 07:46:51 +1000

    Add settings pag

--- app/settings.jsx
+++ app/settings.jsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 export default function SettingsScreen() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Settings</Text>
             {/* Add your settings options here */}
+            <TouchableOpacity
+                style={styles.settingsCard}
+                onPress={() => alert("Start Crawl pressed")}
+            >
+                <Text> Profile </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.settingsCard}
+                onPress={() => alert("Start Crawl pressed")}
+            >
+                <Text> History</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.settingsCard}
+                onPress={() => alert("Start Crawl pressed")}
+            >
+                <Text> Account Settings</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.settingsCard}
+                onPress={() => alert("Start Crawl pressed")}
+            >
+                <Text> Logout </Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -13,11 +37,24 @@ export default function SettingsScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        paddingHorizontal: 15,
+    },
+    settingsCard: {
+        maxHeight: 150,
+        width: "100%",
+        borderWidth: 2,
+        padding: 10,
+        margin: 5,
+        borderRadius: 10,
+
+        display: "flex",
+        flexDirection: "row",
     },
     title: {
         fontSize: 24,
         fontWeight: "bold",
+        marginBottom: 10,
     },
 });
